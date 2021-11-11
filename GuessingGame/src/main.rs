026ecca89e7f9cad25ca26a37c9a105c538fb71d
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-11-10 23:48:51 -0500

    Guessing game complete

--- GuessingGame/src/main.rs
+++ GuessingGame/src/main.rs
@@ -1,14 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let rand = rand::thread_rng().gen_range(1..101); //rand between 1-100
+
     println!("---Guess the number!---");
-    println!("Please input your guess: ");
 
-    let mut guess = String::new();
+    //gameplay loop
+    loop {
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        //read input
+        println!("Please input your guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
+        //compare the rand to guess
+        match guess.cmp(&rand) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("~You win~");
+                break;
+            }
+        }
+        // println!("The secret number is {}", rand);
+    }
 }

fa6cbd9e2096d71e6de7534e69aa1c05c9115d3c
NarishSingh <narish.singh786@gmail.com>
2022-08-27 19:47:17 -0400

    enum work

--- enums-matchs/src/main.rs
+++ enums-matchs/src/main.rs
@@ -1,3 +1,41 @@
+enum IpAddrKind {
+    v4,
+    v6
+}
+
+//struct with an enum
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+
+//putting data directly into an enum
+enum IpAddrEnum {
+    //here, each enum can now act as a function for ctor'ing an instance of that enum
+    V4(String),
+    V6(String)
+}
+
+enum IdAddrEnumInt {
+    V4(u8, u8, u8, u8)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::v4;
+    let six = IpAddrKind::v6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::v4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::v6,
+        address: String::from("::1"),
+    };
+
+    let home2 = IpAddrEnum::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddrEnum::V6(String::from("::1"));
+
+    let home3 = IdAddrEnumInt::V4(127, 0, 0, 1);
 }
---
+++ enums-matchs/target/debug/deps/enums_matchs-b95c668dfba41000.d
@@ -0,0 +1,5 @@
+C:\Users\naris\Documents\Work\TECHHIRE\REPOSITORY\RustWork\enums-matchs\target\debug\deps\enums_matchs-b95c668dfba41000.rmeta: src\main.rs
+
+C:\Users\naris\Documents\Work\TECHHIRE\REPOSITORY\RustWork\enums-matchs\target\debug\deps\enums_matchs-b95c668dfba41000.d: src\main.rs
+
+src\main.rs:
